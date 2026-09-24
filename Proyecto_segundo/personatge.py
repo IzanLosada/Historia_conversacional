3246025b2e30bab04f10a05cdada2122933753c6
XinhaoWu123 <alu.xinhao.wu@mataro.epiaedu.cat>
2026-09-24 17:40:48 +0200

    Update personatge.py

--- Proyecto_segundo/personatge.py
+++ Proyecto_segundo/personatge.py
@@ -4,38 +4,38 @@ class Jugador:
         self.inventari = Inventari()
         self.zona_actual = zona_actual
 
-    def moure(self, zona_nova):
-        self.zona_actual = zona_nova
-        print(f"{self.nom} s'ha mogut a {zona_nova.nom}")
+    def moure(self, nova_zona):
+        if nova_zona in self.zona_actual.sortides:
+            self.zona_actual = nova_zona
+            print(f"T'has mogut a {nova_zona.nom}")
+            nova_zona.mostrar_descripcio()
+        else:
+            print("No pots anar cap allà des d'aquí.")
 
     def agafar(self, objecte):
-        self.zona_actual.objectes.remove(objecte)
-        self.inventari.objectes.append(objecte)
-        objecte.agafar()
+        if objecte in self.zona_actual.objectes:
+            self.zona_actual.eliminar_objecte(objecte)
+            self.inventari.afegir_objecte(objecte)
+            objecte.agafar()
+        else:
+            print(f"Aquí no hi ha cap {objecte.nom}.")
 
     def deixar(self, objecte):
-        self.inventari.objectes.remove(objecte)
-        self.zona_actual.objectes.append(objecte)
-        objecte.deixar()
+        if self.inventari.te_objecte(objecte):
+            self.inventari.eliminar_objecte(objecte)
+            self.zona_actual.afegir_objecte(objecte)
+            objecte.deixar()
+        else:
+            print(f"No portes {objecte.nom} a l'inventari.")
 
     def utilitzar(self, objecte):
-        print(f"{self.nom} utilitza {objecte.nom}")
+        if self.inventari.te_objecte(objecte):
+            objecte.utilitzar()
+        else:
+            print(f"No pots utilitzar {objecte.nom} perquè no el tens.")
 
-    def parlar(self):
-        print(f"{self.nom} vol parlar amb algú")
-
-
-class InventariJugador(Inventari):
-    def __init__(self, capacitat_maxima=5):
-        super().__init__()
-        self.capacitat_maxima = capacitat_maxima
-
-    def afegir_objecte(self, objecte):                              
-
-    def eliminar_objecte(self, objecte):
-      
-    def te_objecte(self, objecte):
-        return objecte in self.objectes
-
-    def mostrar_objectes(self):
-      
+    def parlar(self, personatge):
+        if personatge in self.zona_actual.personatges:
+            personatge.parlar()
+        else:
+            print("Aquí no hi ha ningú amb qui parlar.")
